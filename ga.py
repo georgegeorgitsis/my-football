40f1b4b162c4cd779684b90da28800dc0fc4ee65
George Georgitsis <george.georgitsis@berlinger.com>
2019-05-07 16:28:46 +0200

    changed to tournament selection. finished mating pool and breeding

--- ga.py
+++ ga.py
@@ -1,6 +1,7 @@
 from models.player import Player
 from models.team import Team
 from pymongo import MongoClient
+import numpy as np, random, operator, pandas as pd
 
 try:
     conn = MongoClient()
@@ -30,53 +31,95 @@ def best_teams(population):
     return sorted(population, key=lambda team: team.fitness, reverse=True)
 
 
-def nextGeneration(currentGen, eliteSize, mutationRate):
-    popRanked = best_teams(currentGen)
-    selectionResults = selection(popRanked, eliteSize)
-    matingpool = matingPool(currentGen, selectionResults)
-    children = breedPopulation(matingpool, eliteSize)
-    nextGeneration = mutatePopulation(children, mutationRate)
-    return nextGeneration
+def next_generation(current_gen, elite_size, mutation_rate):
+    pop_ranked = best_teams(current_gen)
+    selection_results = tournament_selection(pop_ranked, elite_size)
+    matingpool = mating_pool(current_gen, selection_results)
+    children = breed_population(matingpool, elite_size)
+    # nextGeneration = mutatePopulation(children, mutation_rate)
+    # return nextGeneration
 
 
-def selection(popRanked, eliteSize):
-    selectionResults = []
-    df = pd.DataFrame(np.array(popRanked), columns=["Index", "Fitness"])
-    df['cum_sum'] = df.Fitness.cumsum()
-    df['cum_perc'] = 100 * df.cum_sum / df.Fitness.sum()
+def tournament_selection(pop_ranked, elite_size):
+    selection_results = []
 
-    for i in range(0, eliteSize):
-        selectionResults.append(popRanked[i][0])
-    for i in range(0, len(popRanked) - eliteSize):
-        pick = 100 * random.random()
-        for i in range(0, len(popRanked)):
-            if pick <= df.iat[i, 3]:
-                selectionResults.append(popRanked[i][0])
-                break
-    return selectionResults
+    for i in range(0, elite_size):
+        selection_results.append(pop_ranked[0])
+    for i in range(0, len(pop_ranked) - elite_size):
+        tournament = []
+        for k in range(0, 3):
+            tournament.append(random.choice(pop_ranked))
+
+        selection_results.append(best_teams(tournament)[0])
+
+    return selection_results
 
 
-def genetic_algorithm(individuals, eliteSize, mutationRate, generations):
+def mating_pool(population, selection_results):
+    mating_pool_list = []
+    for i in range(0, len(selection_results)):
+        index = selection_results[i]
+        mating_pool_list.append(population[index])
+    return mating_pool_list
+
+
+def breed_population(matingpool, elite_size):
+    children = []
+    length = len(matingpool) - elite_size
+    pool = random.sample(matingpool, len(matingpool))
+
+    for i in range(0, elite_size):
+        children.append(matingpool[i])
+
+    for i in range(0, length):
+        child = breed(pool[i], pool[len(matingpool) - i - 1])
+        children.append(child)
+    return children
+
+
+def breed(parent1, parent2):
+    child = []
+    child1 = []
+    child2 = []
+
+    gene_1 = int(random.random() * len(parent1))
+    gene_2 = int(random.random() * len(parent1))
+
+    start_gene = min(gene_1, gene_2)
+    end_gene = max(gene_1, gene_2)
+
+    for i in range(start_gene, end_gene):
+        child1.append(parent1[i])
+
+    child2 = [item for item in parent2 if item not in child1]
+
+    child = child1 + child2
+    return child
+
+
+def genetic_algorithm(individuals, elite_size, mutation_rate, generations):
     population = initial_population(individuals)
 
     print("Initial fitness: " + str(best_teams(population)[0].fitness))
 
     progress = []
     progress.append(best_teams(population)[0].fitness)
-    print(best_teams(population)[0].fitness)
-    # for i in range(0, generations):
-    # pop = nextGeneration(population, eliteSize, mutationRate)
-    #     # print("Generation: " + str(i) + " - distance: " + str(1 / rankRoutes(pop)[0][1]))
-    #     # print("distance: " + str(1 / rankRoutes(pop)[0][1]))
-    #     progress.append(1 / rankRoutes(pop)[0][1])
-    #     last_n = progress[-15:]
-    #     print(last_n.count(last_n[-1]))
-    #     # print('[%s]' % ', '.join(map(str, progress[-10:])))
-    #
-    # print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
-    # bestRouteIndex = rankRoutes(pop)[0][0]
-    # bestRoute = pop[bestRouteIndex]
-    # return bestRoute
+    # print(best_teams(population)[0].fitness)
+    for i in range(0, generations):
+        pop = next_generation(population, elite_size, mutation_rate)
 
 
-genetic_algorithm(individuals=10, eliteSize=20, mutationRate=0.01, generations=20)
+#     # print("Generation: " + str(i) + " - distance: " + str(1 / rankRoutes(pop)[0][1]))
+#     # print("distance: " + str(1 / rankRoutes(pop)[0][1]))
+#     progress.append(1 / rankRoutes(pop)[0][1])
+#     last_n = progress[-15:]
+#     print(last_n.count(last_n[-1]))
+#     # print('[%s]' % ', '.join(map(str, progress[-10:])))
+#
+# print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
+# bestRouteIndex = rankRoutes(pop)[0][0]
+# bestRoute = pop[bestRouteIndex]
+# return bestRoute
+
+
+genetic_algorithm(individuals=20, elite_size=10, mutation_rate=0.01, generations=20)
